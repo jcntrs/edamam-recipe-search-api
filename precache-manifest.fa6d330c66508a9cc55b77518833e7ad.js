7971e0ba32e5ecb8872e22552b64996aeec39854
Jean C. Contreras <j.contreras.open@gmail.com>
2020-03-05 17:58:51 -0300

    Updates

---
+++ precache-manifest.fa6d330c66508a9cc55b77518833e7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb61cbcee22c0a0360b921efac56ce4d",
+    "url": "/edamam-recipe-search-api/index.html"
+  },
+  {
+    "revision": "48ac290d98b4b31733bb",
+    "url": "/edamam-recipe-search-api/static/css/main.74e3e73b.chunk.css"
+  },
+  {
+    "revision": "c71b5a153d9021161726",
+    "url": "/edamam-recipe-search-api/static/js/2.f69ad758.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/edamam-recipe-search-api/static/js/2.f69ad758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ac290d98b4b31733bb",
+    "url": "/edamam-recipe-search-api/static/js/main.a4b0de4a.chunk.js"
+  },
+  {
+    "revision": "b9a1bcc081cfd491d396",
+    "url": "/edamam-recipe-search-api/static/js/runtime-main.2fc13d39.js"
+  }
+]);
